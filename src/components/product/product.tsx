6935681cff655e4335df8a802a4be12ee98d739d
gustavo-marinho1 <gustavobarrosomarinho@gmail.com>
2026-08-26 15:11:12 -0300

    product card

--- src/components/product/product.tsx
+++ src/components/product/product.tsx
@@ -1,13 +1,8 @@
 import { Link } from "react-router-dom"
 import type { ProductType } from "../../lib/products"
-import { currencyBRL } from "../../services/format"
+import { currencyUS } from "../../services/format"
 
-interface Props {
-  product: ProductType,
-  onlyImage?: boolean
-}
-
-const Product = ({ product, onlyImage = false }: Props) => {
+export default function Product({ product, onlyImage = false }: { product: ProductType, onlyImage?: boolean }) {
   return (
     <Link to={`/product/${product.id}`} className="w-full aspect-[4/4] flex flex-col gap-1">
       <div className="bg-neutral-100 w-full aspect-[4/4] relative overflow-hidden">
@@ -22,11 +17,26 @@ const Product = ({ product, onlyImage = false }: Props) => {
       {!onlyImage && (
         <div className="flex flex-col items-center px-2">
           <label className="text-sm font-semibold">{product.name}</label>
-          <label className="text-sm">{currencyBRL(product.price)}</label>
+          <label className="text-sm">{currencyUS(product.price)}</label>
         </div>
       )}
     </Link>
   )
 }
 
-export default Product
+export const ProductGroup = ({ title, children }: { title: string, children: React.ReactNode }) => {
+  return (
+    <div className="flex flex-col items-center gap-3">
+      <h2 className="text-xl font-semibold tracking-tighter">{title}</h2>
+      {children}
+    </div>
+  )
+}
+
+export const ProductGrid = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="w-full grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-x-4 gap-y-8">
+      {children}
+    </div>
+  )
+}
--- src/services/format.ts
+++ src/services/format.ts
@@ -1,3 +1,7 @@
 export function currencyBRL(value: number) {
   return value.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
 }
+
+export function currencyUS(value: number) {
+  return value.toLocaleString('pt-BR', { style: 'currency', currency: 'USD' })
+}
